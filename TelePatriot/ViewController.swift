0e8617bce0edfe4d07a8249f4b3294355202508c
Brent Dunklau <bdunklau@yahoo.com>
2017-11-01 00:18:01 -0500

    working on a slide-out menu.  So far, just have the mechanics working: We can swipe left and right and slide out a menu from either direction

--- TelePatriot/ViewController.swift
+++ TelePatriot/ViewController.swift
@@ -2,24 +2,53 @@
 //  ViewController.swift
 //  TelePatriot
 //
-//  Created by Brent Dunklau on 10/8/17.
+//  Created by Brent Dunklau on 10/31/17.
 //  Copyright © 2017 Brent Dunklau. All rights reserved.
 //
 
 import UIKit
 
+// see:  https://www.youtube.com/watch?v=rafJcqqyS1E
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton1: UIBarButtonItem!
+    
+    @IBOutlet weak var menuButton2: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        
+        sideMenus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sideMenus() {
+        if revealViewController() != nil {
+            menuButton1.target = revealViewController()
+            menuButton1.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275 // how far the menu slides open
+            revealViewController().rightViewRevealWidth = 275 // how far the menu slides open
+            
+            menuButton2.target = revealViewController()
+            menuButton2.action = #selector(SWRevealViewController.rightRevealToggle(_ :))
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
 
+    /*
+    // MARK: - nNavigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
